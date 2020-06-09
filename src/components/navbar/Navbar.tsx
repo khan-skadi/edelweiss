62c21ca24f2c0207ccb3c9835d851edb47eb0861
khan-skadi <kartalov.pt@gmail.com>
2020-06-09 18:21:22 +0200

    add navbar skeleton

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Button,
-  Typography,
-  Avatar
-} from '@material-ui/core';
-import { Menu } from '@material-ui/icons';
+import { AppBar, Toolbar, Typography, Avatar, Button } from '@material-ui/core';
+import Icon from '@material-ui/core/Icon';
+
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
 import eyeOfSkadi from '../../assets/images/eyeOfSkadi.png';
 
 const useStyles = makeStyles((theme) => ({
@@ -28,6 +24,21 @@ const useStyles = makeStyles((theme) => ({
     fontSize: '2.3rem',
     fontFamily: 'Barlow',
     textTransform: 'uppercase'
+  },
+  navLinks: {
+    margin: theme.spacing(1),
+    textTransform: 'capitalize',
+    '&:active': {
+      color: theme.palette.primary.main
+    },
+    '&:hover': {
+      color: theme.palette.primary.main
+    }
+  },
+  navSocial: {
+    margin: '2px',
+    fontSize: '1.8rem',
+    color: theme.palette.primary.main
   }
 }));
 
@@ -38,17 +49,30 @@ const Navbar = () => {
     <>
       <AppBar position="static" style={{ backgroundColor: 'black' }}>
         <Toolbar className={classes.toolbar}>
-          {/* <IconButton
-            edge="start"
-            className={classes.menuButton}
-            aria-label="menu"
-          >
-            <Menu />
-          </IconButton> */}
           <Avatar className={classes.logo} src={eyeOfSkadi} alt="avatar" />
           <Typography className={classes.title}>
             <span style={{ fontWeight: 'bold' }}>Edelweiss</span> Stone
           </Typography>
+          <Button variant="text" color="inherit" className={classes.navLinks}>
+            <Typography variant="h6">Home</Typography>
+          </Button>
+          <Button variant="text" color="inherit" className={classes.navLinks}>
+            <Typography variant="h6">About us</Typography>
+          </Button>
+          <Button variant="text" color="inherit" className={classes.navLinks}>
+            <Typography variant="h6">Services</Typography>
+          </Button>
+          <Button variant="text" color="inherit" className={classes.navLinks}>
+            <Typography variant="h6">Stones</Typography>
+          </Button>
+          <Button variant="text" color="inherit" className={classes.navLinks}>
+            <Typography variant="h6">Testimonials</Typography>
+          </Button>
+          <Button variant="text" color="inherit" className={classes.navLinks}>
+            <Typography variant="h6">Contact</Typography>
+          </Button>
+          <Icon className={classes.navSocial} component={InstagramIcon} />
+          <Icon className={classes.navSocial} component={FacebookIcon} />
         </Toolbar>
       </AppBar>
     </>
