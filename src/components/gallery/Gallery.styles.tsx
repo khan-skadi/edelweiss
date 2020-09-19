c7c057848a5ccfcb649b43b1922ad927cb54e4c5
petre-kartalov <petre.kartalov@symphony.is>
2020-09-19 02:49:32 +0200

    add title gallery

--- src/components/gallery/Gallery.styles.tsx
+++ src/components/gallery/Gallery.styles.tsx
@@ -1,7 +1,58 @@
 import { makeStyles, Theme } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme: Theme) => ({
-  sectionGallery: {}
+  sectionGallery: {
+    padding: "70px 0",
+    position: "relative",
+    display: "block",
+    margin: 0,
+    border: 0,
+    verticalAlign: "baseline",
+    "&:before": {
+      position: "absolute",
+      content: "''",
+      top: 0,
+      borderLeft: "1px solid #ebebeb",
+      height: "100%",
+      left: "41px"
+    }
+  },
+  titleMain: {
+    borderTop: "1px solid #ebebeb",
+    borderBottom: "1px solid #ebebeb"
+  },
+  container: {
+    height: "100%",
+    maxWidth: "1850px",
+    paddingRight: "15px",
+    paddingLeft: "15px",
+    marginRight: "auto",
+    marginLeft: "auto",
+    position: "relative",
+    "&:before": {
+      display: "table",
+      content: "''",
+      boxSizing: "border-box"
+    },
+    "&:after": {
+      clear: "both",
+      display: "table",
+      content: "''"
+    }
+  },
+  title: {
+    margin: 0,
+    padding: 0,
+    border: 0,
+    verticalAlign: "baseline",
+    lineHeight: "43px",
+    textTransform: "capitalize",
+    fontFamily: "'Lato', sans-serif",
+    fontWeight: 700,
+    color: "#333",
+    fontSize: "60px"
+  },
+  galleryWrap: {}
 }));
 
 export default useStyles;
--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -3,14 +3,25 @@ import MiniHeader from "../header/MiniHeader";
 import useStyles from "./Gallery.styles";
 
 const Gallery = () => {
-  const { sectionGallery } = useStyles();
+  const {
+    sectionGallery,
+    titleMain,
+    container,
+    title,
+    galleryWrap
+  } = useStyles();
 
   return (
     <>
       <MiniHeader />
-      <div className={sectionGallery}>
-        <h1>Gallery</h1>
-      </div>
+      <section className={sectionGallery}>
+        <div className={titleMain}>
+          <div className={container}>
+            <h1 className={title}>Gallery</h1>
+          </div>
+        </div>
+        <div className={galleryWrap}></div>
+      </section>
     </>
   );
 };
