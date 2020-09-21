4b04fb517339b809b4eb6840f2baed8f0875bb5d
khan-skadi <kartalov.pt@gmail.com>
2020-09-21 14:17:22 +0200

    fix minor css bug in footer

--- src/components/footer/Footer.styles.tsx
+++ src/components/footer/Footer.styles.tsx
@@ -1,17 +1,17 @@
-import { makeStyles, Theme } from "@material-ui/core/styles";
+import { makeStyles, Theme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) => ({
   footer: {},
   footerDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex"
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'flex'
     }
   },
   footerMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none"
+    display: 'flex',
+    [theme.breakpoints.up('sm')]: {
+      display: 'none'
     }
   }
 }));
