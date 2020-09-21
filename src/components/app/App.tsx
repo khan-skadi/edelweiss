78c99ebdfb482fb2f6822d0042df492a46a4fb64
khan-skadi <kartalov.pt@gmail.com>
2020-09-21 18:56:04 +0200

    minor css fixes and server setup for prod

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import dotenv from "dotenv";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // import { makeStyles } from '@material-ui/core/styles';
 
-import LandingPage from "../landingPage/LandingPage";
-import Footer from "../footer/Footer";
-import Navbar from "../navbar/Navbar";
+import LandingPage from '../landingPage/LandingPage';
+import Footer from '../footer/Footer';
+import Navbar from '../navbar/Navbar';
 
-import RequestQuote from "../requestQuote/RequestQuote";
+import RequestQuote from '../requestQuote/RequestQuote';
 
 // const useStyles = makeStyles((theme) => ({
 //   test: {
@@ -19,6 +20,9 @@ dotenv.config();
 
 const App = () => {
   // const classes = useStyles();
+  useEffect(() => {
+    axios.get('/api/v1/gallery').then((res) => console.log(res.data.data));
+  });
 
   return (
     <BrowserRouter>
--- src/components/projects/Projects.styles.tsx
+++ src/components/projects/Projects.styles.tsx
@@ -1,19 +1,32 @@
-import { makeStyles, Theme } from "@material-ui/core/styles";
+import { makeStyles, Theme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) => ({
   sectionProjects: {
-    paddingTop: "40px"
+    paddingTop: '40px'
   },
   projectsDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex"
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+      backgroundColor: '#f1f5f6',
+      paddingBottom: '110px',
+      borderTop: '1px solid #eaeaea',
+      position: 'relative',
+      '&:before': {
+        position: 'absolute',
+        content: '""',
+        right: '50px',
+        top: 0,
+        width: '1px',
+        height: '100%',
+        background: '#cbcecf'
+      }
     }
   },
   projectsMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none"
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
     }
   }
 }));
